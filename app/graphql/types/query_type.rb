5fa789dd4cc8f3c443b200e2460b3dc24141da12
Daniel Halverson <63027888+dhalverson@users.noreply.github.com>
2020-12-23 20:42:02 -0700

    Adds user and all users query

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,13 +1,20 @@
 module Types
   class QueryType < Types::BaseObject
-    # Add root-level fields here.
-    # They will be entry points for queries on your schema.
+    field :get_users, [Types::UserType], null: false do
+      description 'This will return all the users'
+    end
 
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
+    field :get_user, Types::UserType, null: false do
+      description 'This will return a single user'
+      argument :id, ID, required: true
+    end
+
+    def get_users
+      User.all
+    end
+
+    def get_user(id:)
+      User.find(id)
     end
   end
 end
